0f291532583dc42ba0987e53478f611b74134fda
lenzoro <46078520+lenzoro@users.noreply.github.com>
2019-01-31 15:49:07 -0400

    added photo, buttons and updated html/css

--- client/main.js
+++ client/main.js
@@ -3,20 +3,22 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+
+Template.profile.events({
+  'click .js-Interested'(event, instance) {
+   console.log("You clicked Interested");
   },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+   'click .js-Uninterested'(event, instance) {
+  console.log("You clicked Uninterested");
   },
 });
