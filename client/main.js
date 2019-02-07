1dd9313b18ff38225355cc6424fdd378f81eff8d
lenzoro <46078520+lenzoro@users.noreply.github.com>
2019-02-07 15:42:17 -0400

    added a add user system

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../lib/collections.js';
 
 // Template.hello.onCreated(function helloOnCreated() {
 //   // counter starts at 0
@@ -21,4 +22,21 @@ Template.profile.events({
    'click .js-Uninterested'(event, instance) {
   console.log("You clicked Uninterested");
   },
+  
 });
+
+Template.modal.events({
+	'click .js-saveprofile'(event, instance) {
+	var fname= $('#exampleModal input[name="FirstName"]').val();
+	var lname= $('#exampleModal input[name="LastName"]').val();
+  	var img= $('#exampleModal input[name="Image"]').val();
+  	console.log("The First name is",fname);
+  	console.log("The Last name is",lname);
+  	console.log("The Image name is",img);
+  	//resets the form to be blank
+  	 $('#exampleModal input[name="FirstName"]').val("");
+	 $('#exampleModal input[name="LastName"]').val("");
+  	 $('#exampleModal input[name="Image"]').val("");
+  	$('#exampleModal').modal('hide');
+  }
+})
