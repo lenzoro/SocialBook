70fd44712db9f39b809ec289f5d524f56988352c
lenzoro <devoninniss148@gmail.com>
2019-02-21 14:26:34 -0400

    update

--- client/main.js
+++ client/main.js
@@ -4,39 +4,43 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import '../lib/collections.js';
 
-// Template.hello.onCreated(function helloOnCreated() {
-//   // counter starts at 0
-//   this.counter = new ReactiveVar(0);
-// });
-
-// Template.hello.helpers({
-//   counter() {
-//     return Template.instance().counter.get();
-//   },
-// });
-
-Template.profile.events({
-  'click .js-Interested'(event, instance) {
-   console.log("You clicked Interested");
-  },
-   'click .js-Uninterested'(event, instance) {
-  console.log("You clicked Uninterested");
-  },
-  
+Template.profiles.helpers({//template helpers put data into the template
+	profAll(){
+		return userDB.find({});
+	}
 });
 
-Template.modal.events({
-	'click .js-saveprofile'(event, instance) {
-	var fname= $('#exampleModal input[name="FirstName"]').val();
-	var lname= $('#exampleModal input[name="LastName"]').val();
-  	var img= $('#exampleModal input[name="Image"]').val();
-  	console.log("The First name is",fname);
-  	console.log("The Last name is",lname);
-  	console.log("The Image name is",img);
-  	//resets the form to be blank
-  	 $('#exampleModal input[name="FirstName"]').val("");
-	 $('#exampleModal input[name="LastName"]').val("");
-  	 $('#exampleModal input[name="Image"]').val("");
-  	$('#exampleModal').modal('hide');
-  }
-})
+Template.profiles.events({//template events define and customize user interactions(events) within the template
+	'click .js-Interested'(event, instance) {
+ 		console.log("You clicked Interested");
+ 		var profID = this._id;
+ 		userDB.update({_id:profID}, {$set:{'Interested':Interested+1}});
+  	},
+  	'click .js-Uninterested'(event, instance) {
+  		console.log("You clicked Uninterested");
+  	},
+  	'click .js-delete'(event,instance) {
+  		var profID = this._id;
+  		$("#" + profID).fadeOut("slow","swing",function(){
+  			userDB.remove({_id:this._id}); 	
+  		});
+  	}
+});
+
+Template.addUser.events({
+	'click .js-save'(event, instance) {
+		var fname = $('#exampleModal input[name="firstName"]').val()
+		var lname = $('#exampleModal input[name="lastName"]').val()
+		var img = $('#exampleModal input[name="Image"]').val()
+		if(img ==""){
+			img="headshot.jpg"
+		}
+		console.log("The name is",fname,lname);
+		$('#exampleModal input[name="firstName"]').val('')
+		$('#exampleModal input[name="lastName"]').val('')
+		$('#exampleModal input[name="Image"]').val('')
+		$('#exampleModal').modal('hide');
+		userDB.insert({'firstName':fname,'lastName':lname, 'Image':img});
+  		
+  },
+}); 
